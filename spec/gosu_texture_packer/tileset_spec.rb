3b1ccd1707dc28759a9ed81c2eb0a531a4f572c9
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2014-06-18 07:51:58 +0300

    Rewrite tile size test

--- spec/gosu_texture_packer/tileset_spec.rb
+++ spec/gosu_texture_packer/tileset_spec.rb
@@ -30,26 +30,28 @@ RSpec.describe Gosu::TexturePacker::Tileset do
     end
   end
 
+  # Frame format in JSON:
+  # {"frame"=>{"x"=>129, "y"=>0, "w"=>32, "h"=>32}, "rotated"=>false, "trimmed"=>false,
+  # "spriteSourceSize"=>{"x"=>0, "y"=>0, "w"=>32, "h"=>32}, "sourceSize"=>{"w"=>32, "h"=>32}}
   describe '#frame' do
     subject(:loaded_frame) { instance.frame(target_frame) }
+    let(:source_size) { instance.send(:frames)[target_frame]['sourceSize'] }
 
     it 'loads' do
       expect { subject }.to_not raise_error
     end
 
-    # {"frame"=>{"x"=>129, "y"=>0, "w"=>32, "h"=>32}, "rotated"=>false, "trimmed"=>false,
-    # "spriteSourceSize"=>{"x"=>0, "y"=>0, "w"=>32, "h"=>32}, "sourceSize"=>{"w"=>32, "h"=>32}}
     it 'produces a Gosu::Image' do
       should be_a(Gosu::Image)
     end
 
     it 'produces correctly sized image' do
-      expect(loaded_frame.width).to be instance.send(:frames)[target_frame]['frame']['w']
+      expect(loaded_frame.width).to be source_size['w']
+      expect(loaded_frame.height).to be source_size['h']
     end
 
     it 'caches the image' do
       expect(instance.frame(target_frame)).to eq instance.frame(target_frame)
     end
-
   end
 end
